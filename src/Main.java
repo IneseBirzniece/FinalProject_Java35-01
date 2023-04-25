bd5be9fa518b9b9a6bea4e89d88aa6cba15787d9
Agnese Smiltiņa <agnese.smiltina@gmail.com>
2023-04-25 10:21:08 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String dbURL = "jdbc:mysql://localhost:3306/java35";
+        String username = "root";
+        String password = "$arezgitaPar0le!";
+        Scanner scanner = new Scanner(System.in);
 
+        try (Connection conn = DriverManager.getConnection(dbURL, username, password)){
+            // Test: to be commented out later
+            System.out.println("Connected to database!");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+
+        /* To be inserted in methods:
+        <<accessmodifier methodname>> (Connection conn) throws SQLException{
+        String sql = "SELECT * FROM <<table customers, tools or main>>";
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            }
+         */
     }
 }
