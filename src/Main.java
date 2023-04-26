46fd8f3902c767c69446b683fd4cadce0a9db4a6
Arta Koopa <spridearta29@gmail.com>
2023-04-26 22:33:11 +0300

    pievienoju metodes, lai nolastu pieejamos instr, kuriem laiks ldz servisam ir <24h un metodi, lai parbaudotu vai klients eksist DB

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         String dbURL = "jdbc:mysql://localhost:3306/java35";
         String username = "root";
-        String password = "$arezgitaPar0le!";
+        String password = "";
         Scanner scanner = new Scanner(System.in);
         char again = 'y';
 
@@ -22,67 +22,23 @@ public class Main {
                 // šis ir pagaidām, lai varētu pārbaudīt vai lietas strādā
                 System.out.println("Choose one option (a,b,c,d,e,f)");
                 System.out.println("a - reading tools");
-                System.out.println("b - reading main");
+                System.out.println("b - reading available tools and tools with hours till service <=24");
                 System.out.println("c - inserting tools");
                 System.out.println("d - inserting customers");
                 System.out.println("e - deleting tools");
                 System.out.println("f - deleting customer");
+                System.out.println("g - reading customer");
+                System.out.println("h - checking if Customer exists in DB"); // būs jādzēša ārā, jo veidoku, lai tikai pārbaudotu vai darbojas
 
                 char action = scanner.nextLine().charAt(0);
 
                 if (action == 'd') {
 
-                    System.out.println("Enter your name and surname");
-                    String newName = scanner.nextLine();
-
-                    String newPersonalIDNo;
-                    while (true) {
-                        System.out.println("Enter your personal ID number");
-                        newPersonalIDNo = scanner.nextLine().trim();
-                        if (Pattern.matches("[0-9]{6}-[0-9]{5}", newPersonalIDNo)) {
-                            break; // Exit the loop if the input is valid
-                        } else {
-                            System.out.println("Your inputted personal ID number is not valid, try again");
-                        }
-                    }
-
-                    System.out.println("Enter your phone number");
-                    String newPhoneNumber = scanner.nextLine();
-
-                    insertCustomer(conn, newName, newPersonalIDNo, newPhoneNumber);
+                    insertCustomer(conn, scanner);
 
                 } else if (action == 'c') {
-                    System.out.println("Enter tool category");
-                    String newToolCategory = scanner.nextLine();
 
-                    String newToolID;
-                    while (true) {
-                        System.out.println("Enter tool ID, that contains 3-5 letters and 1-3 numbers");
-                        newToolID = scanner.nextLine().toUpperCase(Locale.ROOT).trim();
-
-                        if (!Pattern.matches("[A-Z]{3,5}[0-9]{1,3}", newToolID)) {
-                            System.out.println("Your inputted tool ID number is not valid, try again");
-                        } else if (toolIdExists(conn, newToolID)) {
-                            System.out.println("The inputted tool ID already exists, try again");
-                        } else {
-                            break;// Exit the loop if the input is valid
-                        }
-                    }
-
-                    System.out.println("Enter tool name");
-                    String newNToolName = scanner.nextLine();
-
-                    System.out.println("Enter tool main parameters");
-                    String newToolParameters = scanner.nextLine();
-
-                    System.out.println("Enter maximum usage hours");
-                    int newUsageHours = scanner.nextInt();
-
-                    // !!! šis nestrādā
-                    System.out.println("Enter tool rental price per day");
-                    String newToolPrice = scanner.nextLine();
-
-                    insertTool(conn, newToolCategory, newToolID, newNToolName, newToolParameters, newUsageHours, newToolPrice);
+                    insertTool(conn, scanner);
 
                 } else if (action == 'e') {
                     while (true) {
@@ -112,9 +68,28 @@ public class Main {
                     readTools(conn);
 
                 } else if (action == 'b') {
-                    readMain(conn);
+                    readAvailableTools(conn);
+
+                } else if (action == 'g') {
+                    readCustomer(conn);
+
+                    // šis būs jāliek citā vietā, if not present in SQL Insert customer
+                }else if (action == 'h') {
+
+                    System.out.println("Enter customers personal ID No.:");
+                    String customerIDNo = scanner.nextLine();
+                    if (!customerExistsInDB(conn, customerIDNo)) {
+                        System.out.println("Customer with personal ID No." + customerIDNo + " don't exists, please input it in DB");
+                        insertCustomer(conn, scanner);
+                    } else {
+                        // dati par Customer ielasās DB main
+                    }
                 }
+
+                System.out.println("Do you want to do something more? y/n");
+                again = scanner.nextLine().charAt(0);
             }
+
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -129,35 +104,81 @@ public class Main {
          */
 
     // metode, ar kuru ievada jaunu klientu
-    public static void insertCustomer(Connection conn, String name, String personalIDNo, String phoneNumber) throws SQLException {
+    public static void insertCustomer(Connection conn, Scanner scanner) throws SQLException {
+        System.out.println("Enter customers name and surname");
+        String newName = scanner.nextLine();
 
-            String sql = "INSERT INTO customers (name, personalIDNo, phoneNumber) VALUES (?, ?, ?)";
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, personalIDNo);
-            preparedStatement.setString(3, phoneNumber);
-
-            int rowInserted = preparedStatement.executeUpdate();
-
-            if (rowInserted > 0){
-                System.out.println("A new customer was inserted successfully");
-            }else {
-                System.out.println("Something went wrong");
+        String newPersonalIDNo;
+        while (true) {
+            System.out.println("Enter customers personal ID number");
+            newPersonalIDNo = scanner.nextLine().trim();
+            if (Pattern.matches("[0-9]{6}-[0-9]{5}", newPersonalIDNo)) {
+                break; // Exit the loop if the input is valid
+            } else {
+                System.out.println("Your inputted personal ID number is not valid, try again");
             }
         }
 
-        // metode, ar kuru tiek ievadīts jauns instruments
-        // !!! nestrādā pēdējais apgalvojums par priceDay
-    public static void insertTool(Connection conn, String category, String ID, String name, String specifications, int serviceHours, String priceDay) throws SQLException {
+        System.out.println("Enter customers phone number");
+        String newPhoneNumber = scanner.nextLine();
+
+
+        String sql = "INSERT INTO customers (name, personalIDNo, phoneNumber) VALUES (?, ?, ?)";
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+        preparedStatement.setString(1, newName);
+        preparedStatement.setString(2, newPersonalIDNo);
+        preparedStatement.setString(3, newPhoneNumber);
+
+        int rowInserted = preparedStatement.executeUpdate();
+
+        if (rowInserted > 0){
+            System.out.println("A new customer was inserted successfully");
+        }else {
+            System.out.println("Something went wrong");
+        }
+    }
+
+    // metode, ar kuru tiek ievadīts jauns instruments
+    // !!! nestrādā pēdējais apgalvojums par priceDay
+    public static void insertTool(Connection conn, Scanner scanner) throws SQLException {
+        System.out.println("Enter tool category");
+        String newToolCategory = scanner.nextLine();
+
+        String newToolID;
+        while (true) {
+            System.out.println("Enter tool ID, that contains 3-5 letters and 1-3 numbers");
+            newToolID = scanner.nextLine().toUpperCase(Locale.ROOT).trim();
+
+            if (!Pattern.matches("[A-Z]{3,5}[0-9]{1,3}", newToolID)) {
+                System.out.println("Your inputted tool ID number is not valid, try again");
+            } else if (toolIdExists(conn, newToolID)) {
+                System.out.println("The inputted tool ID already exists, try again");
+            } else {
+                break;// Exit the loop if the input is valid
+            }
+        }
+
+        System.out.println("Enter tool name");
+        String newNToolName = scanner.nextLine();
+
+        System.out.println("Enter tool main parameters");
+        String newToolParameters = scanner.nextLine();
+
+        System.out.println("Enter maximum usage hours");
+        int newUsageHours = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter tool rental price per day");
+        String newToolPrice = scanner.nextLine();
 
         String sql = "INSERT INTO tools (category, ID, name, specifications, serviceHours, priceDay) VALUES (?, ?, ?, ?, ?, ?)";
         PreparedStatement preparedStatement = conn.prepareStatement(sql);
-        preparedStatement.setString(1, category);
-        preparedStatement.setString(2, ID);
-        preparedStatement.setString(3, name);
-        preparedStatement.setString(4, specifications);
-        preparedStatement.setInt(5, serviceHours);
-        preparedStatement.setString(6, priceDay);
+        preparedStatement.setString(1, newToolCategory);
+        preparedStatement.setString(2, newToolID);
+        preparedStatement.setString(3, newNToolName);
+        preparedStatement.setString(4, newToolParameters);
+        preparedStatement.setInt(5, newUsageHours);
+        preparedStatement.setString(6, newToolPrice);
 
         int rowInserted = preparedStatement.executeUpdate();
 
@@ -238,11 +259,27 @@ public class Main {
         }
 
     }
+    public static void readCustomer(Connection conn) throws SQLException {
+
+        String sql = "SELECT * FROM customers";
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()){
+            String name = resultSet.getString(1);
+            String personalIDNo = resultSet.getString(2);
+            String phoneNumber = resultSet.getString(3);
+
+            String output = "Customer : %s - %s - %s";
+            System.out.println(String.format(output, name, personalIDNo, phoneNumber));
+        }
+
+    }
 
     //metode, lai parādītu Main tabulu
-    public static void readMain(Connection conn) throws SQLException {
+    public static void readAvailableTools(Connection conn) throws SQLException {
 
-        String sql = "SELECT * FROM main";
+        String sql = "SELECT * FROM main WHERE available = 1 || untilService >= 24";
         Statement statement = conn.createStatement();
         ResultSet resultSet = statement.executeQuery(sql);
 
@@ -260,6 +297,17 @@ public class Main {
             String output = ": %s - %s - %s - %s - %s - %s - %s - %s";
             System.out.println(String.format(output, nPk, toolID, toolName, available, untilService, rented, rentedPIN, contacts));
         }
+    }
+
+
+    private static boolean customerExistsInDB(Connection conn, String personalIDNo) throws SQLException {
+        String sql = "SELECT COUNT(*) FROM customers WHERE personalIDNo = ?";
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+        preparedStatement.setString(1, personalIDNo);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        int count = resultSet.getInt(1);
+        return count > 0;
     }
 
 
