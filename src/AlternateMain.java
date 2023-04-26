95124cbd84fc528fbfdabb6bdb12697f3c762dd3
Agnese Smiltiņa <agnese.smiltina@gmail.com>
2023-04-26 23:56:12 +0300

    DB update, DB fails ar aptuveno metožu struktūru un komentāriem, alternatīvs main fails

---
+++ src/AlternateMain.java
@@ -0,0 +1,13 @@
+import javax.xml.crypto.Data;
+import java.util.Scanner;
+import java.sql.Connection;
+
+public class AlternateMain {
+    // Objekts mijiedarbībai ar SQL
+    Database database = new Database();
+    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+    }
+}
