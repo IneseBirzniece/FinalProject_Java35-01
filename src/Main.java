c04149f6c317464f920c76560717905e02a382ef
Arta Koopa <spridearta29@gmail.com>
2023-04-28 14:25:39 +0300

    pievienoju deleteTool un deleteCustomer metodem validaciju

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         String dbURL = "jdbc:mysql://localhost:3306/java35";
         String username = "root";
-        String password = "Arta";
+        String password = "";
         Scanner scanner = new Scanner(System.in);
         char again = 'y';
 
