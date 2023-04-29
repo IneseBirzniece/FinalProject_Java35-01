18c83e76bcbff50023ea9df7793e75c77528a587
Agnese Smiltiņa <agnese.smiltina@gmail.com>
2023-04-29 20:41:40 +0300

    Update metode

--- src/ToolIDSearch.java
+++ src/ToolIDSearch.java
@@ -9,6 +9,8 @@ public class ToolIDSearch {
         try (Connection conn = DriverManager.getConnection(dbURL, username, password)) {
             System.out.println("Connected to database!");
             toolIDSearch(conn);
+
+//            serviceReset(conn);
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -39,11 +41,11 @@ public class ToolIDSearch {
         takenBy.setString(1, toolIDSc);
         ResultSet resultSet4 = takenBy.executeQuery();
 
-
         if (resultSet.next()) {
             System.out.println("ID: " + resultSet.getString(2) + "\t Name: " + resultSet.getString(3));
             if (!resultSet2.next() || resultSet3.next()) {
                 System.out.println("Tool is available");
+
             } else if (resultSet4.next()) {
                 System.out.println("Handed out to:\n"
                         + resultSet4.getString(6) + ", " + resultSet4.getString(7) + ", " + resultSet4.getString(8));
@@ -52,5 +54,36 @@ public class ToolIDSearch {
             System.out.println("ID does not exist");
         }
     }
+//public static void serviceReset (Connection conn) throws SQLException{
+//        // Parāda pie atdošanas, kad aprēķinātais stundu skaits ir mazāks par 24 vai 20 (ka vienojas)
+//    // Kaut kas līdz galam nestrādā, bet jāieliek kopēja kodā, tad vajadzētu strādāt
+//    Scanner scanner = new Scanner(System.in);
+//    String sql = "SELECT * FROM main WHERE untilService <20";
+//    Statement toService = conn.createStatement();
+//    ResultSet resultSet3 = toService.executeQuery(sql);
+//
+//    // Izdrukā brīdinājumu vai ieliek esošo brīdinājuma metodi (tad tikai jautājums Send now?)
+//    System.out.println("Warning: tool " + resultSet3.getString(2) + " has less than <20/24> hours till service. Send to service now? y/n");
+//    char confirm = scanner.nextLine().charAt(0);
+//    scanner.nextLine();
+//    System.out.println("Confirm tool ID: ");
+//    String toolIDsC = scanner.nextLine();
+//
+//    // Atiestata stundas līdz apkopei main tabulā
+//    while(confirm == 'y') {
+//        //Salīdzina SQL esošo ID ar lietotāja apstiprināto. Ja sakrīt, atjaunina
+//        if ((resultSet3.getString(2)).equals(toolIDsC)) {
+//            PreparedStatement toolReset = conn.prepareStatement("UPDATE main SET untilService = 500 WHERE toolID = ?");
+//            toolReset.setString(1, toolIDsC);
+//            if (toolReset.executeUpdate() > 0) {
+//                System.out.printf("Service hours for tool %s have been reset to %d", resultSet3.getString(2), resultSet3.getInt(5));
+//            } else {
+//                System.out.println("Resetting service hours failed");
+//            }
+//        } else {
+//            System.out.println("Something went wrong. Check the tool ID");
+//        }
+//    }
+    
+    }
 
-}
